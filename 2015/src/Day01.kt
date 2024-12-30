a4f804a37fa0860e71024108ddc8ed9e227dd721
Dean Cowley <dean.w.cowley@gmail.com>
2024-12-30 18:01:41 +0000

    Day 01: Not Quite Lisp (part 2)

--- 2015/src/Day01.kt
+++ 2015/src/Day01.kt
@@ -3,6 +3,17 @@ fun main() {
         return input.count { it == '(' } - input.count { it == ')' }
     }
 
+    fun part2(input: String): Int {
+        var floor = 0
+        input.forEachIndexed { i, c ->
+            floor += if (c == '(') 1 else -1
+            if (floor == -1) {
+                return i + 1
+            }
+        }
+        return -1
+    }
+
     check(part1("(())") == 0)
     check(part1("()()") == 0)
     check(part1("(((") == 3)
@@ -13,4 +24,8 @@ fun main() {
     check(part1(")))") == -3)
     check(part1(")())())") == -3)
     part1(readInputText("Day01")).println()
+
+    check(part2(")") == 1)
+    check(part2("()())") == 5)
+    part2(readInputText("Day01")).println()
 }
