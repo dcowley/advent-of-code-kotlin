04a029ed2e52340d498136ee96dd4ecd1af8d16f
Dean Cowley <dean.w.cowley@gmail.com>
2024-12-31 07:20:04 +0000

    Day 02: I Was Told There Would Be No Math (part 2)

--- 2015/src/Day02.kt
+++ 2015/src/Day02.kt
@@ -11,7 +11,24 @@ fun main() {
         }
     }
 
+    fun part2(input: List<String>): Int {
+        return input.sumOf { line ->
+            val dimensions = "\\d+".toRegex().findAll(line)
+                .flatMap(MatchResult::groupValues)
+                .map(String::toInt)
+                .toList()
+
+            dimensions.sorted().let {
+                2 * (it[0] + it[1]) + it[0] * it[1] * it[2]
+            }
+        }
+    }
+
     check(part1(listOf("2x3x4")) == 58)
     check(part1(listOf("1x1x10")) == 43)
     part1(readInput("Day02")).println()
+
+    check(part2(listOf("2x3x4")) == 34)
+    check(part2(listOf("1x1x10")) == 14)
+    part2(readInput("Day02")).println()
 }
