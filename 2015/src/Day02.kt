d664ca40e8b7aba6a125ba757272370a49731a6f
Dean Cowley <dean.w.cowley@gmail.com>
2024-12-31 07:39:45 +0000

    Day 02: I Was Told There Would Be No Math (refactor)

--- 2015/src/Day02.kt
+++ 2015/src/Day02.kt
@@ -1,34 +1,28 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.sumOf { line ->
-            val dimensions = "\\d+".toRegex().findAll(line)
-                .flatMap(MatchResult::groupValues)
-                .map(String::toInt)
-                .toList()
+    data class Dimensions(val l: Int, val w: Int, val h: Int)
 
-            val areas = listOf(dimensions[0] * dimensions[1], dimensions[0] * dimensions[2], dimensions[1] * dimensions[2])
-            2 * areas.sum() + areas.min()
-        }
+    fun parse(input: List<String>): List<Dimensions> = input.map { line ->
+        val values = "\\d+".toRegex().findAll(line)
+            .flatMap { it.groupValues }
+            .toList()
+        Dimensions(values[0].toInt(), values[1].toInt(), values[2].toInt())
     }
 
-    fun part2(input: List<String>): Int {
-        return input.sumOf { line ->
-            val dimensions = "\\d+".toRegex().findAll(line)
-                .flatMap(MatchResult::groupValues)
-                .map(String::toInt)
-                .toList()
-
-            dimensions.sorted().let {
-                2 * (it[0] + it[1]) + it[0] * it[1] * it[2]
-            }
-        }
+    fun part1(input: List<Dimensions>) = input.sumOf { (l, w, h) ->
+        val areas = listOf(l * w, l * h, w * h)
+        2 * areas.sum() + areas.min()
     }
 
-    check(part1(listOf("2x3x4")) == 58)
-    check(part1(listOf("1x1x10")) == 43)
-    part1(readInput("Day02")).println()
+    fun part2(input: List<Dimensions>) = input.sumOf { (l, w, h) ->
+        val sides = listOf(l, w, h).sorted()
+        2 * (sides[0] + sides[1]) + (l * w * h)
+    }
 
-    check(part2(listOf("2x3x4")) == 34)
-    check(part2(listOf("1x1x10")) == 14)
-    part2(readInput("Day02")).println()
+    check(part1(parse(listOf("2x3x4"))) == 58)
+    check(part1(parse(listOf("1x1x10"))) == 43)
+    part1(parse(readInput("Day02"))).println()
+
+    check(part2(parse(listOf("2x3x4"))) == 34)
+    check(part2(parse(listOf("1x1x10"))) == 14)
+    part2(parse(readInput("Day02"))).println()
 }
