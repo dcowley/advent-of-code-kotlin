5fa2f631ff4ef13d7f5a45eddcde82523dd50c5b
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-01 15:04:43 +0000

    Day 04: The Ideal Stocking Stuffer (part 2)

--- 2015/src/Day04.kt
+++ 2015/src/Day04.kt
@@ -1,14 +1,19 @@
 fun main() {
-    fun part1(input: String): Long {
+    fun solve(input: String, numZeros: Int): Long {
         var i = -1L
         while (true) {
-            if ("$input${++i}".md5().startsWith("0".repeat(5))) {
+            if ("$input${++i}".md5().startsWith("0".repeat(numZeros))) {
                 return i
             }
         }
     }
 
+    fun part1(input: String) = solve(input, 5)
+    fun part2(input: String) = solve(input, 6)
+
     check(part1("abcdef") == 609043L)
     check(part1("pqrstuv") == 1048970L)
     part1(readInputText("Day04")).println()
+
+    part2(readInputText("Day04")).println()
 }
