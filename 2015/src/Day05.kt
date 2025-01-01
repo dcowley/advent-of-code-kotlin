2993240ade4a90c6555a2f380016ce6ed8b63a63
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-01 17:10:22 +0000

    Day 05: Doesn't He Have Intern-Elves For This? (part 2)

--- 2015/src/Day05.kt
+++ 2015/src/Day05.kt
@@ -1,17 +1,31 @@
 fun main() {
-    fun String.hasVowels() = "[aeiou]".toRegex().findAll(this).count() >= 3
-    fun String.hasRepeats() = zipWithNext { a, b -> a == b }.any { it }
-    fun String.isValid() = "ab|cd|pq|xy".toRegex().findAll(this).count() == 0
-    fun String.isNice() = hasVowels() && hasRepeats() && isValid()
-    fun String.isNaughty() = !isNice()
+    fun part1(input: List<String>): Int {
+        fun String.hasVowels() = "[aeiou]".toRegex().findAll(this).count() >= 3
+        fun String.hasRepeats() = zipWithNext { a, b -> a == b }.any { it }
+        fun String.isValid() = "ab|cd|pq|xy".toRegex().findAll(this).count() == 0
+        fun String.isNice() = hasVowels() && hasRepeats() && isValid()
 
-    fun part1(input: List<String>) = input.count(String::isNice)
+        return input.count(String::isNice)
+    }
 
-    check("ugknbfddgicrmopn".isNice())
-    check("aaa".isNice())
-    check("jchzalrnumimnmhp".isNaughty())
-    check("haegwjzuvuyypxyu".isNaughty())
-    check("dvszwmarrgswjxmb".isNaughty())
+    fun part2(input: List<String>): Int {
+        return input.count {
+            val repeats = it.zipWithNext { a, b -> "$a$b".toRegex().findAll(it).count() > 1 }.any { it }
+            val surrounds = it.zipWithNext { a, b -> "$a$b$a".toRegex().findAll(it).count() >= 1 }.any { it }
+            repeats && surrounds
+        }
+    }
 
+    check(part1(listOf("ugknbfddgicrmopn")) == 1)
+    check(part1(listOf("aaa")) == 1)
+    check(part1(listOf("jchzalrnumimnmhp")) == 0)
+    check(part1(listOf("haegwjzuvuyypxyu")) == 0)
+    check(part1(listOf("dvszwmarrgswjxmb")) == 0)
     part1(readInput("Day05")).println()
+
+    check(part2(listOf("qjhvhtzxzqqjkmpb")) == 1)
+    check(part2(listOf("xxyxx")) == 1)
+    check(part2(listOf("uurcxstgmygtbstg")) == 0)
+    check(part2(listOf("ieodomkazucvgmuy")) == 0)
+    part2(readInput("Day05")).println()
 }
