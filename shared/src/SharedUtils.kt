771b72d6cb7eeba12ab02ab3246dd4eab81298a4
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-01 12:42:54 +0000

    Day 03: Perfectly Spherical Houses in a Vacuum (refactor)

--- shared/src/SharedUtils.kt
+++ shared/src/SharedUtils.kt
@@ -2,3 +2,33 @@
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+typealias Node = Pair<Int, Int>
+
+val Node.x
+    get() = first
+val Node.y
+    get() = second
+
+fun Node.move(direction: Direction) = when (direction) {
+    Direction.EAST -> x + 1 to y
+    Direction.SOUTH -> x to y + 1
+    Direction.WEST -> x - 1 to y
+    Direction.NORTH -> x to y - 1
+}
+
+enum class Direction {
+    NORTH, EAST, SOUTH, WEST;
+
+    fun turn(rotation: Rotation) = when (this) {
+        EAST -> if (rotation == Rotation.CLOCKWISE) SOUTH else NORTH
+        SOUTH -> if (rotation == Rotation.CLOCKWISE) WEST else EAST
+        WEST -> if (rotation == Rotation.CLOCKWISE) NORTH else SOUTH
+        NORTH -> if (rotation == Rotation.CLOCKWISE) EAST else WEST
+    }
+}
+
+enum class Rotation {
+    CLOCKWISE,
+    ANTICLOCKWISE
+}
