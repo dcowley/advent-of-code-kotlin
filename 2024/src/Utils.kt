bc3bfcaeed58c87e9c60aaa8eab2f457dc894bfd
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-01 15:03:00 +0000

    Day 04: The Ideal Stocking Stuffer (part 1)

--- 2024/src/Utils.kt
+++ 2024/src/Utils.kt
@@ -1,8 +1,5 @@
-import java.math.BigInteger
-import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
-
 
 /**
  * Reads lines from the given input txt file.
@@ -13,13 +10,6 @@ fun readInputText(name: String) = Path("2024/src/$name.txt").readText().trim()
  * Reads lines from the given input txt file.
  */
 fun readInput(name: String) = readInputText(name).lines()
-
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
 
 fun String.toGrid(): Map<Pair<Int, Int>, Char> {
     val w = indexOf('\n')
--- shared/src/SharedUtils.kt
+++ shared/src/SharedUtils.kt
@@ -1,7 +1,17 @@
+import java.math.BigInteger
+import java.security.MessageDigest
+
 /**
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Converts string to md5 hash.
+ */
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+    .toString(16)
+    .padStart(32, '0')
 
 typealias Node = Pair<Int, Int>
 
