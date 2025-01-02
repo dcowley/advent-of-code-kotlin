594009c898fb66f3c0a8827132e54e49abc119df
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-02 07:01:03 +0000

    Day 06: Probably a Fire Hazard (part 1)

---
+++ 2015/src/Day06.kt
@@ -0,0 +1,35 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val lights = buildMap {
+            repeat(1000) { row ->
+                repeat(1000) { col ->
+                    this[col to row] = false
+                }
+            }
+        }.toMutableMap()
+
+        val regex = "(turn on|turn off|toggle) (\\d+,\\d+) through (\\d+,\\d+)".toRegex()
+        input.forEach {
+            val matches = regex.findAll(it).flatMap(MatchResult::groupValues).toList()
+            val instruction = matches[1]
+            val startNode = matches[2].split(',').let { pair -> pair[0].toInt() to pair[1].toInt() }
+            val endNode = matches[3].split(',').let { pair -> pair[0].toInt() to pair[1].toInt() }
+
+            for (x in (startNode.first..endNode.first)) {
+                for (y in startNode.second..endNode.second) {
+                    when (instruction) {
+                        "turn on" -> lights[x to y] = true
+                        "turn off" -> lights[x to y] = false
+                        "toggle" -> lights[x to y] = !lights.getValue(x to y)
+                    }
+                }
+            }
+        }
+        return lights.count { it.value }
+    }
+
+    check(part1(listOf("turn on 0,0 through 999,999")) == 1000000)
+    check(part1(listOf("toggle 0,0 through 999,0")) == 1000)
+    check(part1(listOf("turn off 499,499 through 500,500")) == 0)
+    part1(readInput("Day06")).println()
+}
