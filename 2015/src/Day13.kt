b84374ca41ecca0073fad6c4df6fbea8b31442d5
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-04 09:49:34 +0000

    Day 13: Knights of the Dinner Table (part 1)

---
+++ 2015/src/Day13.kt
@@ -0,0 +1,36 @@
+fun main() {
+    fun parse(name: String) = buildMap<String, MutableSet<Pair<String, Int>>> {
+        val input = readInput(name).map {
+            val matches = "(\\w+) would (gain|lose) (\\d+) happiness units by sitting next to (\\w+).".toRegex().find(it)!!.groupValues
+            matches[1] to (matches[4] to matches[3].toInt() * if (matches[2] == "gain") 1 else -1)
+        }
+
+        input.forEach { (guest, neighbour) ->
+            val gtn = input.first { it.first == neighbour.first && it.second.first == guest }
+            getOrPut(guest, ::mutableSetOf) += (neighbour.first to neighbour.second + gtn.second.second)
+        }
+    }
+
+    fun <T> permutations(list: List<T>): List<List<T>> = when {
+        list.size == 1 -> listOf(list)
+        else ->
+            permutations(list.drop(1))
+                .flatMap { perm ->
+                    (list.indices).map { i ->
+                        perm.subList(0, i) + list.first() + perm.drop(i)
+                    }
+                }
+    }
+
+    fun part1(graph: Map<String, MutableSet<Pair<String, Int>>>): Int {
+        val permutations = permutations(graph.keys.toList())
+        return permutations.maxOf { seating ->
+            (seating + seating.first())
+                .zipWithNext { guest1, guest2 -> graph.getValue(guest1).first { it.first == guest2 }.second }
+                .sum()
+        }
+    }
+
+    check(part1(parse("Day13_test")) == 330)
+    part1(parse("Day13")).println()
+}
