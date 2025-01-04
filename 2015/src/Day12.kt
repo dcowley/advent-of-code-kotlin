6a5db5c557545b658dacb88f5e88a3fe92d8ab29
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-04 07:31:57 +0000

    Day 12: JSAbacusFramework.io (part 2)

--- 2015/src/Day12.kt
+++ 2015/src/Day12.kt
@@ -1,8 +1,46 @@
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+
 fun main() {
     fun part1(input: String): Int {
         return "(-*\\d+)".toRegex().findAll(input).sumOf {
             it.groupValues.first().toInt()
         }
+    }
+
+    fun part2(input: String): Int {
+        val fromJson = Gson().fromJson(input, JsonElement::class.java)
+
+        fun solve(json: JsonElement): Int {
+            return when {
+                json.isJsonPrimitive -> {
+                    return if (json.asJsonPrimitive.isNumber) {
+                        json.asJsonPrimitive.asInt
+                    } else 0
+                }
+
+                json.isJsonObject -> {
+                    val entries = json.asJsonObject.entrySet()
+                    return if (entries.any { it.value.isJsonPrimitive && it.value.asString == "red" }) {
+                        0
+                    } else {
+                        entries.sumOf {
+                            solve(it.value)
+                        }
+                    }
+                }
+
+                json.isJsonArray -> {
+                    json.asJsonArray.sumOf {
+                        solve(it)
+                    }
+                }
+
+                else -> error("Invalid JsonElement type: $json")
+            }
+        }
+
+        return solve(fromJson)
     }
 
     check(part1("[1,2,3]") == 6)
@@ -14,4 +52,10 @@ fun main() {
     check(part1("[]") == 0)
     check(part1("{}") == 0)
     part1(readInputText("Day12")).println()
+
+    check(part2("[1,2,3]") == 6)
+    check(part2("[1,{\"c\":\"red\",\"b\":2},3]") == 4)
+    check(part2("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}") == 0)
+    check(part2("[1,\"red\",5]") == 6)
+    part2(readInputText("Day12")).println()
 }
