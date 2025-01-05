ac9602fa8fe8bc4fccce69e84cd8c116dd5e04d3
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-05 09:32:01 +0000

    Day 15: Science for Hungry People (refactor)

--- 2015/src/Day15.kt
+++ 2015/src/Day15.kt
@@ -1,18 +1,7 @@
 fun main() {
-    data class Qualities(
-        val capacity: Int,
-        val durability: Int,
-        val flavor: Int,
-        val texture: Int,
-        val calories: Int
-    )
-
-    val regex = "(\\w+): \\w* (-?\\d+), \\w* (-?\\d+), \\w* (-?\\d+), \\w* (-?\\d+), \\w* (-?\\d+)".toRegex()
-    fun parse(name: String) = readInput(name).associate { line ->
-        regex.find(line)!!.let {
-            val (ingredient, capacity, durability, flavor, texture, calories) = it.destructured
-            ingredient to Qualities(capacity.toInt(), durability.toInt(), flavor.toInt(), texture.toInt(), calories.toInt())
-        }
+    val regex = "\\w+: \\w* (-?\\d+), \\w* (-?\\d+), \\w* (-?\\d+), \\w* (-?\\d+), \\w* (-?\\d+)".toRegex()
+    fun parse(name: String) = readInput(name).map { line ->
+        regex.find(line)!!.destructured.toList().map(String::toInt)
     }
 
     fun permutations(total: Int, num: Int): List<List<Int>> = when {
@@ -29,55 +18,27 @@ fun main() {
         }
     }
 
-    fun part1(input: Map<String, Qualities>, teaspoons: Int = 100): Long {
-        return permutations(teaspoons, input.keys.size).maxOf {
-            val capacity = input.keys.zip(it)
-                .sumOf { (ingredient, quantity) -> input.getValue(ingredient).capacity * quantity }
-                .coerceAtLeast(0)
-            val durability = input.keys.zip(it)
-                .sumOf { (ingredient, quantity) -> input.getValue(ingredient).durability * quantity }
-                .coerceAtLeast(0)
-            val flavor = input.keys.zip(it)
-                .sumOf { (ingredient, quantity) -> input.getValue(ingredient).flavor * quantity }
-                .coerceAtLeast(0)
-            val texture = input.keys.zip(it)
-                .sumOf { (ingredient, quantity) -> input.getValue(ingredient).texture * quantity }
-                .coerceAtLeast(0)
-
-            val result = (capacity * durability * flavor * texture.toLong())
-            result
-        }
-    }
-
-    fun part2(input: Map<String, Qualities>, teaspoons: Int = 100): Long {
-        return permutations(teaspoons, input.keys.size).maxOf {
-            val calories = input.keys.zip(it)
-                .sumOf { (ingredient, quantity) -> input.getValue(ingredient).calories * quantity }
-                .coerceAtLeast(0)
-            if (calories != 500) {
-                0L
-            } else {
-                val capacity = input.keys.zip(it)
-                    .sumOf { (ingredient, quantity) -> input.getValue(ingredient).capacity * quantity }
-                    .coerceAtLeast(0)
-                val durability = input.keys.zip(it)
-                    .sumOf { (ingredient, quantity) -> input.getValue(ingredient).durability * quantity }
-                    .coerceAtLeast(0)
-                val flavor = input.keys.zip(it)
-                    .sumOf { (ingredient, quantity) -> input.getValue(ingredient).flavor * quantity }
-                    .coerceAtLeast(0)
-                val texture = input.keys.zip(it)
-                    .sumOf { (ingredient, quantity) -> input.getValue(ingredient).texture * quantity }
-                    .coerceAtLeast(0)
-
-                (capacity * durability * flavor * texture.toLong())
+    fun solve(input: List<List<Int>>, teaspoons: Int = 100) = permutations(teaspoons, input.size).map {
+        val totals = input.foldIndexed(emptyList<Int>()) { ingredient, acc, qualities ->
+            qualities.mapIndexed { index, quality ->
+                quality * it[ingredient] + acc.getOrElse(index) { 0 }
             }
         }
+
+        totals.last() to totals.dropLast(1).reduce { acc, total ->
+            acc.coerceAtLeast(0) * total
+        }
     }
 
-    check(part1(parse("Day15_test")) == 62_842_880L)
+    fun part1(input: List<List<Int>>, teaspoons: Int = 100) = solve(input, teaspoons).maxOf { it.second }
+
+    fun part2(input: List<List<Int>>, teaspoons: Int = 100) = solve(input, teaspoons)
+        .filter { it.first == 500 }
+        .maxOf { it.second }
+
+    check(part1(parse("Day15_test")) == 62_842_880)
     part1(parse("Day15")).println()
 
-    check(part2(parse("Day15_test")) == 57_600_000L)
+    check(part2(parse("Day15_test")) == 57_600_000)
     part2(parse("Day15")).println()
 }
