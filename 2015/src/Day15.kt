98fda4e9f50cfd49673f148b1df55cb99f41c91d
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-05 09:24:16 +0000

    Day 15: Science for Hungry People (part 1)

---
+++ 2015/src/Day15.kt
@@ -0,0 +1,54 @@
+fun main() {
+    data class Qualities(
+        val capacity: Int,
+        val durability: Int,
+        val flavor: Int,
+        val texture: Int,
+        val calories: Int
+    )
+
+    val regex = "(\\w+): \\w* (-?\\d+), \\w* (-?\\d+), \\w* (-?\\d+), \\w* (-?\\d+), \\w* (-?\\d+)".toRegex()
+    fun parse(name: String) = readInput(name).associate { line ->
+        regex.find(line)!!.let {
+            val (ingredient, capacity, durability, flavor, texture, calories) = it.destructured
+            ingredient to Qualities(capacity.toInt(), durability.toInt(), flavor.toInt(), texture.toInt(), calories.toInt())
+        }
+    }
+
+    fun permutations(total: Int, num: Int): List<List<Int>> = when {
+        num == 0 -> emptyList()
+        num == 1 -> listOf(listOf(total))
+        else -> {
+            buildList {
+                repeat(total + 1) { i ->
+                    permutations(total - i, num - 1).forEach {
+                        add(listOf(i) + it)
+                    }
+                }
+            }
+        }
+    }
+
+    fun part1(input: Map<String, Qualities>, teaspoons: Int = 100): Long {
+        return permutations(teaspoons, input.keys.size).maxOf {
+            val capacity = input.keys.zip(it)
+                .sumOf { (ingredient, quantity) -> input.getValue(ingredient).capacity * quantity }
+                .coerceAtLeast(0)
+            val durability = input.keys.zip(it)
+                .sumOf { (ingredient, quantity) -> input.getValue(ingredient).durability * quantity }
+                .coerceAtLeast(0)
+            val flavor = input.keys.zip(it)
+                .sumOf { (ingredient, quantity) -> input.getValue(ingredient).flavor * quantity }
+                .coerceAtLeast(0)
+            val texture = input.keys.zip(it)
+                .sumOf { (ingredient, quantity) -> input.getValue(ingredient).texture * quantity }
+                .coerceAtLeast(0)
+
+            val result = (capacity * durability * flavor * texture.toLong())
+            result
+        }
+    }
+
+    check(part1(parse("Day15_test")) == 62_842_880L)
+    part1(parse("Day15")).println()
+}
