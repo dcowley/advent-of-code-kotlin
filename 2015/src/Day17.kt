5a96a613dee784a46ef087181bd9d29ff4238d3f
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-06 11:17:52 +0000

    Day 17: No Such Thing as Too Much (part 2)

--- 2015/src/Day17.kt
+++ 2015/src/Day17.kt
@@ -1,10 +1,15 @@
+import kotlin.math.min
+
 fun main() {
     fun parse(name: String) = readInput(name).map(String::toInt)
 
-    fun part1(input: List<Int>, litres: Int): List<Collection<Int>> {
-        val powerSet = input.powerSet()
-        val filter = powerSet.filter { it.sum() == litres }
-        return filter
+    fun solve(input: List<Int>, litres: Int) = input.powerSet().filter { it.sum() == litres }
+
+    fun part1(input: List<Int>, litres: Int) = solve(input, litres)
+
+    fun part2(input: List<Int>, litres: Int) = solve(input, litres).let { combos ->
+        val minSize = combos.minOf(List<Int>::size)
+        combos.filter { it.size == minSize }
     }
 
     part1(parse("Day17_test"), 25).let { result ->
@@ -12,6 +17,8 @@ fun main() {
         check(result.count { it.sorted() == listOf(5, 5, 15) } == 1)
         check(result.count { it == listOf(5, 20) } == 2)
     }
-
     part1(parse("Day17"), 150).size.println()
+
+    check(part2(parse("Day17_test"), 25).size == 3)
+    part2(parse("Day17"), 150).size.println()
 }
