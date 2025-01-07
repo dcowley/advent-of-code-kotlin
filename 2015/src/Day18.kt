e6002b0a748c6d3baedc6e15b9ddc8f9571f79ac
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-07 09:39:54 +0000

    Day 18: Like a GIF For Your Yard (refactor)

--- 2015/src/Day18.kt
+++ 2015/src/Day18.kt
@@ -3,23 +3,23 @@ fun main() {
 
     fun Pair<Int, Int>.neighbours() = (first - 1..first + 1).flatMap { x -> (second - 1..second + 1).map { y -> x to y } } - this
 
-    fun part1(grid: Map<Pair<Int, Int>, Char>, steps: Int): Map<Pair<Int, Int>, Char> {
-        var state = grid
-        repeat(steps) {
-            state = state.toMutableMap().apply {
-                state.keys.forEach { key ->
-                    val neighboursOn = key.neighbours()
-                        .filter { it in state.keys }
-                        .count { state[it] == '#' }
-                    if (state[key] == '#') {
-                        this[key] = if (neighboursOn in 2..3) '#' else '.'
-                    } else {
-                        this[key] = if (neighboursOn == 3) '#' else '.'
-                    }
-                }
+    fun solve(grid: Map<Pair<Int, Int>, Char>, region: Set<Pair<Int, Int>>) = grid.toMutableMap().apply {
+        region.forEach { key ->
+            val neighboursOn = key.neighbours()
+                .filter { it in grid.keys }
+                .count { grid[it] == '#' }
+
+            this[key] = when (grid[key]) {
+                '#' -> if (neighboursOn in 2..3) '#' else '.'
+                else -> if (neighboursOn == 3) '#' else '.'
             }
         }
-        return state
+    }
+
+    fun part1(grid: Map<Pair<Int, Int>, Char>, steps: Int): Map<Pair<Int, Int>, Char> = grid.toMutableMap().apply {
+        repeat(steps) {
+            putAll(solve(this, this.keys))
+        }
     }
 
     fun part2(grid: Map<Pair<Int, Int>, Char>, steps: Int): Map<Pair<Int, Int>, Char> {
@@ -27,24 +27,12 @@ fun main() {
         val maxY = grid.maxOf { it.key.second }
         val corners = setOf(0 to 0, maxX to 0, 0 to maxY, maxX to maxY)
 
-        var state: Map<Pair<Int, Int>, Char> = grid.toMutableMap().apply {
-            corners.forEach { this[it] = '#' }
-        }
-        repeat(steps) {
-            state = state.toMutableMap().apply {
-                (state.keys - corners).forEach { key ->
-                    val neighboursOn = key.neighbours()
-                        .filter { it in state.keys }
-                        .count { state[it] == '#' }
-                    if (state[key] == '#') {
-                        this[key] = if (neighboursOn in 2..3) '#' else '.'
-                    } else {
-                        this[key] = if (neighboursOn == 3) '#' else '.'
-                    }
-                }
+        return grid.toMutableMap().apply {
+            putAll(corners.map { it to '#' })
+            repeat(steps) {
+                putAll(solve(this, keys - corners))
             }
         }
-        return state
     }
 
     part1(parse("Day18_test"), 4).let {
@@ -72,16 +60,4 @@ fun main() {
         check(it == state)
     }
     part2(parse("Day18"), 100).values.count { it == '#' }.println()
-}
-
-fun Map<Pair<Int, Int>, Char>.pp() {
-    val cols = keys.minOf { it.first }..keys.maxOf { it.first }
-    val rows = keys.minOf { it.second }..keys.maxOf { it.second }
-
-    for (row in rows) {
-        for (col in cols) {
-            print(this[col to row])
-        }
-        print('\n')
-    }
 }
