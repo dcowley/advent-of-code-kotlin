04c86afc3c1fe94c9e0247c78bfeaa2902bf3e09
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-15 18:10:41 +0000

    Day 23: Opening the Turing Lock (part 1)

---
+++ 2015/src/Day24.kt
@@ -0,0 +1,21 @@
+import kotlin.math.pow
+
+fun main() {
+    fun part1(): Int {
+        var a = (2 * (3.0.pow(7) + 3.0.pow(4) + 3.0.pow(3)) + 1).toInt()
+        var b = 0
+
+        while (a != 1) {
+            b += 1
+            if (a % 2 == 0) {
+                a /= 2
+            } else {
+                a *= 3
+                a += 1
+            }
+        }
+        return b
+    }
+
+    part1().println()
+}
