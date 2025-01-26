a103a9bde5cb919c1aa94c4ca6b9cf309098982a
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-26 13:02:58 +0000

    Day 24: It Hangs in the Balance (part 2)

--- 2015/src/Day24.kt
+++ 2015/src/Day24.kt
@@ -46,6 +46,26 @@ fun main() {
         error("No solution")
     }
 
+    fun part2(input: Set<Int>): Long {
+        val target = input.sum() / 4
+
+        var size = 1
+        while (size < input.size) {
+            val combinations = combinations(input.toIntArray(), size)
+                .filter { it.sum() == target }
+
+            if (combinations.count() > 0) {
+                return combinations
+                    .map { it.fold(1L) { acc, i -> acc * i } }
+                    .min()
+            }
+            size++
+        }
+
+        error("No solution")
+    }
+
     check(part1(parse("Day24_test")) == 99L)
     part1(parse("Day24")).println()
+    part2(parse("Day24")).println()
 }
