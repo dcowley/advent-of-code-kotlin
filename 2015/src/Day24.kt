e6b1da9c9a3cec7c7643d23e33d9460d7adef5ef
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-26 13:06:33 +0000

    Day 24: It Hangs in the Balance (refactor)

--- 2015/src/Day24.kt
+++ 2015/src/Day24.kt
@@ -1,69 +1,26 @@
-import java.util.*
-
 fun main() {
     fun parse(name: String) = readInput(name)
         .map(String::toInt)
         .toSet()
 
-    fun combinations(input: IntArray, size: Int) = sequence {
-        val result = IntArray(size)
-        val stack = LinkedList<Int>().apply {
-            push(0)
-        }
-
-        while (stack.isNotEmpty()) {
-            var inputIndex = stack.pop()
-            var resultIndex = stack.size
-
-            while (inputIndex < input.size) {
-                result[resultIndex++] = input[inputIndex++]
-                stack.push(inputIndex)
-
-                if (resultIndex == size) {
-                    yield(result)
-                    break
-                }
-            }
-        }
-    }
-
-    fun part1(input: Set<Int>): Long {
-        val target = input.sum() / 3
-
+    fun solve(input: Set<Int>, target: Int): Long {
         var size = 1
         while (size < input.size) {
             val combinations = combinations(input.toIntArray(), size)
                 .filter { it.sum() == target }
 
-            if (combinations.count() > 0) {
+            if (combinations.isNotEmpty()) {
                 return combinations
-                    .map { it.fold(1L) { acc, i -> acc * i } }
-                    .min()
+                    .minOf { it.fold(1L) { acc, i -> acc * i } }
             }
             size++
         }
-
         error("No solution")
     }
 
-    fun part2(input: Set<Int>): Long {
-        val target = input.sum() / 4
+    fun part1(input: Set<Int>) = solve(input, input.sum() / 3)
 
-        var size = 1
-        while (size < input.size) {
-            val combinations = combinations(input.toIntArray(), size)
-                .filter { it.sum() == target }
-
-            if (combinations.count() > 0) {
-                return combinations
-                    .map { it.fold(1L) { acc, i -> acc * i } }
-                    .min()
-            }
-            size++
-        }
-
-        error("No solution")
-    }
+    fun part2(input: Set<Int>) = solve(input, input.sum() / 4)
 
     check(part1(parse("Day24_test")) == 99L)
     part1(parse("Day24")).println()
