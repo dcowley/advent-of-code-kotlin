cbc2785480bede5ec06b6784e55c8e3d8015f812
Dean Cowley <dean.w.cowley@gmail.com>
2025-04-28 08:15:37 +0100

    Day 02: Bathroom Security (part 2)

--- 2016/src/Day02.kt
+++ 2016/src/Day02.kt
@@ -1,10 +1,4 @@
 fun main() {
-    val keypad = """
-        123
-        456
-        789
-    """.trimIndent().toGrid()
-
     val directions = mapOf(
         'U' to Direction.NORTH,
         'D' to Direction.SOUTH,
@@ -13,15 +7,45 @@ fun main() {
     )
 
     fun part1(input: List<String>) = buildString {
+        val keypad = """
+            123
+            456
+            789
+        """.trimIndent().toGrid()
+
         var position = 1 to 1
 
         input.forEach { sequence ->
             sequence.forEach {
                 val next = position.move(directions.getValue(it))
-                position = next.copy(
-                    next.x.coerceIn(0, 2),
-                    next.y.coerceIn(0, 2)
-                )
+                if (next in keypad.keys) {
+                    position = next
+                }
+            }
+
+            append(keypad[position])
+        }
+    }
+
+    fun part2(input: List<String>) = buildString {
+        val keypad = """
+            |  1  
+            | 234 
+            |56789
+            | ABC 
+            |  D  
+        """.trimMargin()
+            .toGrid()
+            .filterValues { it.isLetterOrDigit() }
+
+        var position = 0 to 2
+
+        input.forEach { sequence ->
+            sequence.forEach {
+                val next = position.move(directions.getValue(it))
+                if (next in keypad.keys) {
+                    position = next
+                }
             }
 
             append(keypad[position])
@@ -30,4 +54,7 @@ fun main() {
 
     check(part1(readInput("Day02_test")) == "1985")
     println(part1(readInput("Day02")))
+
+    check(part2(readInput("Day02_test")) == "5DB3")
+    println(part2(readInput("Day02")))
 }
