bbce94d3d41b58447257a92263f7977beea2fcdf
Dean Cowley <dean.w.cowley@gmail.com>
2025-05-14 14:38:06 +0100

    Day 03: Squares With Three Sides (part 2)

--- 2016/src/Day03.kt
+++ 2016/src/Day03.kt
@@ -10,5 +10,27 @@ fun main() {
         sides[0] + sides[1] > sides[2]
     }
 
+    fun part2(input: String): Int {
+        val numbers = Regex("\\d+")
+            .findAll(input)
+            .map { it.value.toInt() }
+            .toList()
+
+        var count = 0
+        repeat(3) { col ->
+            val indices = generateSequence(col) { i -> (i + 3).takeIf { it in numbers.indices } }
+
+            count += numbers.slice(indices.asIterable())
+                .windowed(3, 3)
+                .map { it.sorted() }
+                .count { it[0] + it[1] > it[2] }
+        }
+
+        return count
+    }
+
     println(part1(readInput("Day03")))
+
+    check(part2(readInputText("Day03_test")) == 6)
+    println(part2(readInputText("Day03")))
 }
