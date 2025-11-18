9f244efac7d359b67bd737abdc320b4e96197730
Dean Cowley <dean.w.cowley@gmail.com>
2025-11-18 17:56:06 +0000

    2021/01 Sonar Sweep (part 2)

--- 2021/src/Day01.kt
+++ 2021/src/Day01.kt
@@ -1,10 +1,20 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.map(String::toInt)
+    fun parse(input: List<String>) = input.map(String::toInt)
+    fun part1(input: List<Int>): Int {
+        return input
             .zipWithNext()
             .count { (a, b) -> a < b }
     }
 
-    check(part1(readInput("Day01_test")) == 7)
-    println(part1(readInput("Day01")))
+    fun part2(input: List<Int>): Int {
+        return input
+            .windowed(3)
+            .zipWithNext()
+            .count { (l, r) -> l.sum() < r.sum() }
+    }
+
+    check(part1(parse(readInput("Day01_test"))) == 7)
+    check(part2(parse(readInput("Day01_test"))) == 5)
+    println(part1(parse(readInput("Day01"))))
+    println(part2(parse(readInput("Day01"))))
 }
