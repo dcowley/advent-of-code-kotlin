b1c1bc323d47363e9131ff755220ff4d693e8064
Dean Cowley <dean.w.cowley@gmail.com>
2025-12-01 20:08:32 +0000

    ♻️  2025/01/R Secret Entrance

--- 2025/src/Day01.kt
+++ 2025/src/Day01.kt
@@ -6,6 +6,14 @@ private data class Rotation(val direction: Char, val clicks: Int, val revolution
             'L' -> 100 - clicks
             else -> clicks
         }
+
+    companion object {
+        fun fromRawValue(rawValue: String) = Rotation(
+            direction = rawValue.first(),
+            clicks = rawValue.drop(1).toInt() % 100,
+            revolutions = rawValue.drop(1).toInt() / 100
+        )
+    }
 }
 
 private data class Dial(
@@ -14,33 +22,21 @@ private data class Dial(
 )
 
 object Day01 {
-    private fun parse(input: String) = input.lines()
+    private fun parse(input: String) = input
+        .lines()
+        .map(Rotation::fromRawValue)
 
-    fun part1(input: String) = parse(input).let {
-        var position = 50
-
-        it.count { instruction ->
-            val next = instruction.drop(1).toInt()
-            when (instruction.first()) {
-                'L' -> position -= next
-                'R' -> position += next
-            }
-
-            position % 100 == 0
+    fun part1(input: String) = parse(input)
+        .fold(Dial()) { dial, rotation ->
+            dial.copy(
+                value = (dial.value + rotation.distance) % 100,
+                counter = dial.counter + if ((dial.value + rotation.distance) % 100 == 0) 1 else 0
+            )
         }
-    }
+        .counter
 
-    fun part2(input: String): Int {
-        val rotations = parse(input)
-            .map {
-                Rotation(
-                    direction = it.first(),
-                    clicks = it.drop(1).toInt() % 100,
-                    revolutions = it.drop(1).toInt() / 100
-                )
-            }
-
-        return rotations.fold(Dial()) { dial, rotation ->
+    fun part2(input: String) = parse(input)
+        .fold(Dial()) { dial, rotation ->
             dial.copy(
                 value = (dial.value + rotation.distance) % 100,
                 counter = dial.counter + rotation.revolutions
@@ -51,8 +47,8 @@ object Day01 {
                             else -> 0
                         }
             )
-        }.counter
-    }
+        }
+        .counter
 }
 
 suspend fun main() {
