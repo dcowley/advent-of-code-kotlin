3edcd897a5ccc8bbc6751aa678e122b53cef0937
Dean Cowley <dean.w.cowley@gmail.com>
2025-12-05 12:10:49 +0000

    🧩 2025/05 Cafeteria

---
+++ 2025/test/Day05Test.kt
@@ -0,0 +1,28 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day05Test {
+    private val testData = """
+        3-5
+        10-14
+        16-20
+        12-18
+
+        1
+        5
+        8
+        11
+        17
+        32
+    """.trimIndent()
+
+    @Test
+    fun part1() {
+        assertEquals(3, Day05.part1(testData))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(14, Day05.part2(testData))
+    }
+}
